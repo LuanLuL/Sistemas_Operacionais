7ed9191ca705a19cd218d07cd56ea0e572e4f2f9
Luan Gonçalves Santos <luanlulu2010@hotmail.com>
2024-12-09 14:00:20 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// "./dataset/codigo.txt"
+
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "Portuguese");
+    cout << "\nBegin of this project!\n\n";
+    return 0;
+}
